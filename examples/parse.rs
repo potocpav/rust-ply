92081fef1a38b0eeb17bfaee73fbdd697f481cbe
Pavel Potocek <pavelpotocek@gmail.com>
2015-01-26 15:23:19 +0100

    Moved an example Element spec to examples, where it belongs.

--- examples/parse.rs
+++ examples/parse.rs
@@ -8,8 +8,37 @@ extern crate ply;
 #[derive(Debug)]
 #[ply_data]
 struct S {
-	vertex: Vec<ply::Position>,
-	face: Vec<ply::Position>,
+	vertex: Vec<Position>,
+	face: Vec<Position>,
+}
+
+
+#[derive(Debug, Copy)]
+pub struct Position {
+	x: f32, y: f32, z: f32
+}
+
+impl ply::Element for Position {
+	fn check(_dummy: Option<Self>, spec: &ply::ElementSpec) -> Result<(), &'static str> {
+		if spec.props.len() != 3 {
+			return Err("Wrong number of params.")
+		}
+		for prop in spec.props.iter() {
+			if prop.type_ != ply::Type::Float && prop.type_ != ply::Type::Double {
+				return Err("Wrong type of params.");
+			}
+		}
+		Ok(())
+	}
+
+	fn parse(line: &Vec<String>) -> Result<Self,&'static str> {
+		let n: Vec<f32> = line.iter().filter_map(|a| a.parse()).collect();
+		if n.len() != 3 {
+			Err("Wrong number of elements on a line.")
+		} else {
+			Ok(Position { x: n[0], y: n[1], z: n[2] })
+		}
+	}
 }
 
 
@@ -30,7 +59,8 @@ fn main() {
 			let model: Result<S, &'static str> = ply::PlyModel::new(&ply);
 			println!("\nResult: {:?}", model);
 
-		}, Err(e) => println!("Error: {}", e),
+		},
+		Err(e) => println!("Error: {}", e),
 	}
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -27,31 +27,3 @@ pub trait Element {
 	fn check(Option<Self>, &parser::ElementSpec) -> Result<(), &'static str>;
 	fn parse(&Vec<String>) -> Result<Self,&'static str>;
 }
-
-#[derive(Debug, Copy)]
-pub struct Position {
-	x: f32, y: f32, z: f32
-}
-
-impl Element for Position {
-	fn check(_dummy: Option<Self>, spec: &parser::ElementSpec) -> Result<(), &'static str> {
-		if spec.props.len() != 3 {
-			return Err("Wrong number of params.")
-		}
-		for prop in spec.props.iter() {
-			if prop.type_ != Type::Float && prop.type_ != Type::Double {
-				return Err("Wrong type of params.");
-			}
-		}
-		Ok(())
-	}
-
-	fn parse(line: &Vec<String>) -> Result<Self,&'static str> {
-		let n: Vec<f32> = line.iter().filter_map(|a| a.parse()).collect();
-		if n.len() != 3 {
-			Err("Wrong number of elements on a line.")
-		} else {
-			Ok(Position { x: n[0], y: n[1], z: n[2] })
-		}
-	}
-}
