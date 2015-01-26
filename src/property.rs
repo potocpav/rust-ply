fd1e150978bc23a924aba805c6b8a39ae661aab1
Pavel Potocek <pavelpotocek@gmail.com>
2015-01-26 14:46:18 +0100

    Added a property file, that I earlier forgot :/

---
+++ src/property.rs
@@ -0,0 +1,20 @@
+
+
+
+
+pub trait Property {
+	fn get_type() -> super::Type;
+}
+
+macro_rules! property_type { ($t:ty, $i:ident) => (
+    impl Property for $t {
+		fn get_type() -> super::Type {
+			super::Type::$i
+		}
+	}
+)}
+
+property_type!{i32, Int}
+property_type!{u32, UInt}
+property_type!{f32, Float}
+property_type!{f64, Double}
