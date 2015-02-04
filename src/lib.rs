89caf69086273327e37d7c1e0b6c97c6f81d600b
Pavel Potocek <pavelpotocek@gmail.com>
2015-02-04 15:57:08 +0100

    Made the function `Element::parse_one` optional (with a run-time error if neither `parse` nor `parse_one` is specified).

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,11 @@ pub trait Model {
 pub trait Element: Sized {
 	// dummy parameter until UFCS
 	fn check(Option<Self>, &ElementSpec) -> Result<(),String>;
-	fn parse_one(&Vec<String>) -> Result<Self,String>;
+
+	fn parse_one(&Vec<String>) -> Result<Self,String> {
+		Err("`parse_one` must be implemented when used by the `parse` function.".to_string())
+	}
+
 	fn parse(e: &ElementSpec) -> Result<Vec<Self>,String> {
 		let mut res = Vec::with_capacity(e.data.len());
 		for l in e.data.iter() {
